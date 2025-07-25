f2598d3268075ab0d531a736595328839c94a783
Adityashekar <150983280+AdhiShekar07@users.noreply.github.com>
2025-07-25 18:12:55 +0530

    Add files via upload
    ✅ Day 1 - Added Reverse Array using Temp Array [Low]

---
+++ Day01/GfG.java
@@ -0,0 +1,24 @@
+// Problem: Reverse Array using Temporary Array
+// Difficulty: Low
+// Language: Java
+
+import java.util.Arrays;
+
+class ReverseArrayTemp {
+    
+    static void reverseArray(int[] arr) {
+        int n = arr.length;
+        int[] temp = new int[n];
+        for (int i = 0; i < n; i++)
+            temp[i] = arr[n - i - 1];
+        for (int i = 0; i < n; i++)
+            arr[i] = temp[i];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 3, 2, 6, 5 };
+        reverseArray(arr);
+        for (int i = 0; i < arr.length; i++) 
+            System.out.print(arr[i] + " ");
+    }
+}
